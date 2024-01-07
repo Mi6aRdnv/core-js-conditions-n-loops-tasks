d99cb05adc301ac54d4e348c513928dc04d236ba
Mi6aRdnv <kvberty@gmail.com>
2024-01-07 20:15:25 +0100

    feat: implement getIndexOf function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -271,8 +271,15 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let charIndex = -1;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      charIndex = i;
+    }
+  }
+
+  return charIndex;
 }
 
 /**
